85b7fe938bd41c84885d1626119e84c2235cac60
dagram777 <88400066+dagram777@users.noreply.github.com>
2024-03-08 16:54:23 +0300

    Update Create Create LAB-1-Control Flow- exercise-4.dart

--- Create Create LAB-1-Control Flow- exercise-4.dart
+++ Create Create LAB-1-Control Flow- exercise-4.dart
@@ -1,1 +1,27 @@
+import 'dart:io';
+
+void main() {
+  print('Type in your grade:');
+  int grade = int.parse(stdin.readLineSync()!);
+
+  switch (grade) {
+    case 10:
+    case 9:
+      grade = 'A';
+      break;
+    case 8:
+      grade = 'B';
+      break;
+    case 7:
+      grade = 'C';
+      break;
+    case 6:
+      grade = 'D';
+      break;
+    default:
+      grade = 'F';
+  }
+
+  print('Your grade is: $grade');
+}
 
