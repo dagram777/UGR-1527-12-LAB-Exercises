05e6b10383a0ba66f63d0a6450cdadef72d99596
dagram777 <88400066+dagram777@users.noreply.github.com>
2024-03-08 16:45:40 +0300

    Create LAB-1-Control Flow- exercise-1.dart

---
+++ LAB-1-Control Flow- exercise-1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  
+  print('Type  number:');
+  int number = int.parse(stdin.readLineSync()!);
+
+  
+  if (number % 2 == 0) {
+    print('$number is an even number.');
+  } else {
+    print('$number is an odd number.');
+  }
+}
