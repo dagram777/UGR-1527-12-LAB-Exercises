042c1060ca7da8b99458e5352b6eb13478132a66
dagram777 <88400066+dagram777@users.noreply.github.com>
2024-03-08 16:31:52 +0300

    Update LAB-1-Variables-and-data-types.dart

--- LAB-1-Variables-and-data-types.dart
+++ LAB-1-Variables-and-data-types.dart
@@ -1,1 +1,14 @@
+void main() {
+  
+  // Variable Declration
+  String favColor = 'green';
+  String name = 'Abebe';
+  int age = 10;
+
+  
+ // Printing to console
+  print('Color Favorite: $favColor');
+  print('Name: $name');
+  print('Age: $age');
+}
 
